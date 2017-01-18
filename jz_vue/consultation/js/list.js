2bcb1781a281131a56988eaa8e34e3d87545efd9
MJIN <316462327@qq.com>
2017-01-18 16:59:12 +0800

    列表

---
+++ jz_vue/consultation/js/list.js
@@ -0,0 +1,35 @@
+
+var list = new Vue({
+    el: '#app',
+    data: {
+        pagenum: 1,
+        fromorgid: 'HTL00000002',
+        fromdeptid: '56',
+        doctorid: '83e9b2f76cdd4a1fbcebb9dc2830fc70',
+        pmList: {
+            username:"",//患者姓名
+            doctorid:"",
+            fromorgid:"",//医院id
+            fromdeptid:"",
+            toorgid:"",
+            todeptid:"",//科室id
+            consultationparticipantname: '',//会诊参与人医生专家姓名
+            starttime:'',//"2016-11-21 17:17:38",//开始时间
+            endtime:''//"2016-11-28 17:17:38",//结束时间
+        }
+    }
+    ready: function(){
+        this.cbList();
+    },
+    methods: {
+        cbList: function(){
+            var self = this;
+            getImJson.post('/v1/telemedicine/getTelemedicineList.json?pagesize=10&pagenum=',{})
+                     .then(function(res){
+                        var json = res.data;
+                        self.pmList = json;
+                     })
+                     .catch(function(){});
+        }
+    }
+});
