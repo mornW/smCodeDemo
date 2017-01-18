d5b34f2a9f3af4961d22da1f9e61579f591c25a7
MJIN <316462327@qq.com>
2017-01-18 16:03:34 +0800

    ajax 赋值

---
+++ demo1/js/demo1.js
@@ -0,0 +1,48 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        // classA: 'class-a',
+//      classB: 'class-b',
+//      isA: false,
+//      isB: true,
+        pmUser: {
+            username:'',
+            age: '',
+            genter: '',
+            dateofbirth: '',
+            address:'',
+            idcard: '',
+            mobile1: '',
+            admissionsScene: '',
+            patientdescription: '',
+            medicalhistory: '',
+            clinicaldiagnosis: '',
+            consciousness: '',
+            pupil: '',
+            lightReflection: '',
+            skin: '',
+            externalCauses: '',
+            prognosisOfHospital: '',
+            visitsResults: ''
+        }
+        // jobs: [
+        //      {text: '退休', value: '10'},
+        //      {text: '工人', value: '20'}
+        // ]
+    },
+    ready: function () {//初始化完成后执行的函数
+        this.cbUser();
+    },
+    methods:{
+        cbUser: function(){
+            var self = this;
+            getImJson.post('/v1/firstaid/getfirstAidinfo', {taskcode: '4cb7d68cce2e462d82220bfc413da2b9'})
+                     .then(function (res) {
+                        var json = res.data;
+                        self.pmUser = json;
+                        console.log(self.pmUser);
+                    }).catch(function (error) {});
+        }
+    }
+})
